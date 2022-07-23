d587bc14e9083e9a669e07b77b7350d51eb43d63
marco12-star <99267663+Anvar2742@users.noreply.github.com>
2022-07-23 18:20:02 +0200

    Game ready

--- app.js
+++ app.js
@@ -1,32 +1,95 @@
 const startBtn = document.querySelector('#start')
 const screens = document.querySelectorAll('.screen')
-const timeList = document.querySelector('.time-list')
+const timeList = document.querySelector('#timeList')
+const menu = document.querySelector('.menu')
+const menuTime = document.querySelector('#menuTime')
+const menuBar = document.querySelector('.menu__bar')
+const close = document.querySelector('.close')
 const timeEl = document.querySelector('#time')
+const timeBtns = document.querySelectorAll('.menu .time-btn')
 const board = document.querySelector('#board')
+const realtimeScore = document.querySelector('.realtime-score')
+const topPlayersTable = document.querySelector('.top__players')
+const registr = document.querySelector('.registr')
+const nameInput = document.querySelector('#name')
 const startAudio = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3")
 const audio = new Audio("tap.mp3")
 const screenBorad = document.querySelector('.screen-board')
+let chosenTime = 0
 let time = 0
 let score = 0
-var gameInterval
+let scoreFirst = 0
+let scoreSec = 0
+let scoreThird = 0
+let gameInterval
+
+// Check if user is already registered
+const cookieName = getCookie('name')
+if (cookieName) {
+    registr.innerHTML = `<h2>Welcome, ${cookieName}!</h2>`
+}
 
 startBtn.addEventListener('click', (event) => {
     event.preventDefault()
-    screens[0].classList.add('up')
-    startAudio.play()
+    let name
+    if (cookieName) {
+        name = cookieName
+    } else {
+        name = nameInput.value
+    }
+    if (name) {
+        screens[0].classList.add('up')
+        startAudio.play()
+        localStorage.setItem('name', name);
+        // Apply setCookie
+        setCookie('name', name, 30);
+    }
 })
 
 timeList.addEventListener('click', (event) => {
     if (event.target.classList.contains('time-btn')) {
         time = +(event.target.getAttribute('data-time'))
+        chosenTime = +(event.target.getAttribute('data-time'))
         screens[1].classList.add('up')
         startGame()
     }
 })
 
+menuTime.addEventListener('click', (event) => {
+    if (event.target.classList.contains('time-btn')) {
+        chosenTime = +(event.target.getAttribute('data-time'))
+
+        timeBtns.forEach(el => {
+            el.classList.remove('active')
+        });
+        event.target.classList.add('active')
+
+        getData()
+    }
+})
+
+menuBar.addEventListener('click', (event) => {
+    menu.classList.add('active')
+
+    if (chosenTime !== 0) {
+        timeBtns.forEach(el => {
+            el.classList.remove('active')
+
+            if (+(el.getAttribute('data-time')) === chosenTime) {
+                el.classList.add('active')
+            }
+        });
+    }
+})
+
+close.addEventListener('click', (event) => {
+    menu.classList.remove('active')
+})
+
 board.addEventListener('click', event => {
     if (event.target.classList.contains('circle')) {
         score++
+        realtimeScore.querySelector('.primary').innerHTML = score
         event.target.remove()
         createRandomCircle()
         audio.play()
@@ -46,6 +109,8 @@ function startGame() {
         score = 0
         timeEl.parentElement.classList.remove('remove')
     }
+
+    realtimeScore.classList.remove('hide')
     startAudio.play()
     createRandomCircle()
 }
@@ -71,6 +136,16 @@ function finishGame() {
     board.innerHTML = `<h1>Score: <span class="primary">${score}</span></h1><button class="btn try_btn">Try Again!</button>`
     clearInterval(gameInterval)
 
+    if (chosenTime === +(timeBtns[0].getAttribute('data-time'))) {
+        scoreFirst = score
+        localStorage.setItem('scoreFirst', scoreFirst);
+    } else if (chosenTime === +(timeBtns[1].getAttribute('data-time'))) {
+        scoreSec = score
+        localStorage.setItem('scoreSec', scoreSec);
+    } else {
+        scoreThird = score
+        localStorage.setItem('scoreThird', scoreThird);
+    }
 
     const tryAgain = document.querySelector('.try_btn')
     tryAgain.addEventListener('click', event => {
@@ -80,6 +155,10 @@ function finishGame() {
         }
         startAudio.play()
     })
+
+    realtimeScore.classList.add('hide')
+    realtimeScore.querySelector('.primary').innerHTML = 0
+    getData()
 }
 
 function createRandomCircle() {
@@ -105,6 +184,118 @@ function getRandomNum(min, max) {
     return Math.round(Math.random() * (max - min) + min)
 }
 
-fetch('https://jsonplaceholder.typicode.com/todos/1') //api for the get request
-    .then(response => response.json())
-    .then(data => console.log(data));
+const getData = () => {
+    fetch('https://aim-game-a9de7-default-rtdb.europe-west1.firebasedatabase.app/db.json') //api for the get request
+        .then(response => response.json())
+        .then(data => {
+            let match = data.findIndex(player => player.name == localStorage.getItem('name'))
+
+            if (chosenTime === +(timeBtns[0].getAttribute('data-time'))) {
+                if (match !== -1 && data[match].scoreFirst < scoreFirst) {
+                    data[match].scoreFirst = scoreFirst
+                }
+            } else if (chosenTime === +(timeBtns[1].getAttribute('data-time'))) {
+                if (match !== -1 && data[match].scoreSec < scoreSec) {
+                    data[match].scoreSec = scoreSec
+                }
+            } else {
+                if (match !== -1 && data[match].scoreThird < scoreThird) {
+                    data[match].scoreThird = scoreThird
+                }
+            }
+            if (match == -1) {
+                let playerObj = {
+                    id: 3,
+                    name: localStorage.getItem('name'),
+                    scoreFirst: scoreFirst,
+                    scoreSec: scoreSec,
+                    scoreThird: scoreThird
+                }
+                data.push(playerObj)
+            }
+            // db sort
+            function byField(field) {
+                return (a, b) => a[field] > b[field] ? -1 : 1;
+            }
+
+            // Table render
+            topPlayersTable.querySelectorAll('tr').forEach(el => {
+                el.remove()
+            })
+
+            topPlayersTable.querySelector('thead').insertAdjacentHTML('beforeEnd', `
+                <tr>
+                    <th>Rank</th>
+                    <th>Name</th>
+                    <th>Best Score</th>
+                </tr>
+            `)
+
+            if (chosenTime === 10) {
+                data.sort(byField('scoreFirst'));
+                data.forEach((player, index) => {
+                    topPlayersTable.querySelector('tbody').insertAdjacentHTML('beforeEnd', `
+                    <tr>
+                        <td>${++index}</td>
+                        <td>${player.name}</td>
+                        <td>${player.scoreFirst !== undefined ? player.scoreFirst : 0}</td>
+                    </tr>
+                `)
+                })
+            } else if (chosenTime === 20) {
+                data.sort(byField('scoreSec'));
+                data.forEach((player, index) => {
+                    topPlayersTable.querySelector('tbody').insertAdjacentHTML('beforeEnd', `
+                        <tr>
+                            <td>${++index}</td>
+                            <td>${player.name}</td>
+                            <td>${player.scoreSec !== undefined ? player.scoreSec : 0}</td>
+                        </tr>
+                    `)
+                })
+            } else {
+                data.sort(byField('scoreThird'));
+                data.forEach((player, index) => {
+                    topPlayersTable.querySelector('tbody').insertAdjacentHTML('beforeEnd', `
+                        <tr>
+                            <td>${++index}</td>
+                            <td>${player.name}</td>
+                            <td>${player.scoreThird !== undefined ? player.scoreThird : 0}</td>
+                        </tr>
+                    `)
+                })
+            }
+            // db post
+            postData(data)
+        });
+
+
+}
+
+
+function postData(data) {
+    fetch('https://aim-game-a9de7-default-rtdb.europe-west1.firebasedatabase.app/db.json', {
+        method: 'PUT',
+        body: JSON.stringify(data), // The data
+    }).then((res) => res.json())
+}
+
+// Set a Cookie
+function setCookie(cName, cValue, expDays) {
+    let date = new Date();
+    date.setTime(date.getTime() + (expDays * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = cName + "=" + cValue + "; " + expires + "; path=/";
+}
+
+// Get a cookie
+function getCookie(cName) {
+    const name = cName + "=";
+    const cDecoded = decodeURIComponent(document.cookie); //to be careful
+    const cArr = cDecoded.split('; ');
+    let res;
+    cArr.forEach(val => {
+        if (val.indexOf(name) === 0) res = val.substring(name.length);
+    })
+    return res;
+}
